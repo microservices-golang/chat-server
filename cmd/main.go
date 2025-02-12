1f0d9bbdf33ba0ce35c99c063eb7d63d1b25e84c
Denis <denis@MacBook-Pro-Denis.local>
2025-02-13 00:19:51 +0300

    lesson_1

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"github.com/microservices-golang/chat-server/pkg/chat"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+type server struct {
+	chat.UnimplementedChatServiceServer
+}
+
+var grpcPort = 50052
+
+// protoc  --go_out=./pkg/ --go-grpc_out=./pkg api/chat_v1/chat.proto
+func (s *server) CreateChat(_ context.Context, req *chat.CreateChatRequest) (*chat.CreateChatResponse, error) {
+	fmt.Println(req.Usernames)
+	return &chat.CreateChatResponse{Id: 1}, nil
+}
+
+func (s *server) DeleteChat(_ context.Context, req *chat.DeleteChatRequest) (*emptypb.Empty, error) {
+	fmt.Println(req.Id)
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(_ context.Context, req *chat.SendMessageRequest) (*emptypb.Empty, error) {
+	fmt.Printf("SendMessage request: from=%s, text=%s, timestamp=%v\n", req.From, req.Text, req.Timestamp)
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat.RegisterChatServiceServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/microservices-golang/chat-server
 
 go 1.23
 
@@ -7,5 +7,11 @@ require github.com/fatih/color v1.17.0
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
