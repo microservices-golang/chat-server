cd6bcc38024fedadb2fa32aa5062eb5c66bb2300
Denis <denis@MacBook-Pro-Denis.local>
2025-02-14 23:55:11 +0300

    фикс после ревью

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,11 @@ import (
 	"log"
 	"net"
 
-	"github.com/microservices-golang/chat-server/pkg/chat"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/microservices-golang/chat-server/pkg/chat"
 )
 
 type server struct {
@@ -18,17 +19,19 @@ type server struct {
 
 var grpcPort = 50052
 
-// protoc  --go_out=./pkg/ --go-grpc_out=./pkg api/chat_v1/chat.proto
+// CreateChat - создание нового чата
 func (s *server) CreateChat(_ context.Context, req *chat.CreateChatRequest) (*chat.CreateChatResponse, error) {
-	fmt.Println(req.Usernames)
+	fmt.Println(req.UserEmails)
 	return &chat.CreateChatResponse{Id: 1}, nil
 }
 
+// DeleteChat - удаление чата
 func (s *server) DeleteChat(_ context.Context, req *chat.DeleteChatRequest) (*emptypb.Empty, error) {
 	fmt.Println(req.Id)
 	return &emptypb.Empty{}, nil
 }
 
+// SendMessage - отправка сообщения в чат
 func (s *server) SendMessage(_ context.Context, req *chat.SendMessageRequest) (*emptypb.Empty, error) {
 	fmt.Printf("SendMessage request: from=%s, text=%s, timestamp=%v\n", req.From, req.Text, req.Timestamp)
 	return &emptypb.Empty{}, nil
